fbe661860ab2fc487c01bad49b711d18a7c58af9
Sam Collier <samcollier916@gmail.com>
2026-02-04 15:58:18 +0000

    log to OSREPORT as well as video console

--- src/log.zig
+++ src/log.zig
@@ -9,8 +9,10 @@ pub fn log(
 ) void {
     if (scope == std.log.default_log_scope) {
         _ = std.c.printf("%s: ", @tagName(level).ptr);
+        c.SYS_Report("%s: ", @tagName(level).ptr);
     } else {
         _ = std.c.printf("%s(%s): ", @tagName(level).ptr, @tagName(scope).ptr);
+        c.SYS_Report("%s(%s): ", @tagName(level).ptr, @tagName(scope).ptr);
     }
 
     const ArgsType = @TypeOf(args);
@@ -34,10 +36,12 @@ pub fn log(
             i += 1;
         } else {
             _ = c.putchar(format[i]);
+            c.SYS_Report("%c", format[i]);
         }
     }
 
     _ = c.putchar('\n');
+    c.SYS_Report("\n");
 }
 
 fn printValue(value: anytype) void {
@@ -46,24 +50,39 @@ fn printValue(value: anytype) void {
     switch (@typeInfo(T)) {
         .int => {
             if (@typeInfo(T).int.signedness == .signed) {
-                _ = std.c.printf("%lld", @as(c_longlong, value));
+                const converted_val = @as(c_longlong, value);
+                _ = std.c.printf("%lld", converted_val);
+                c.SYS_Report("%lld", converted_val);
             } else {
-                _ = std.c.printf("%llu", @as(c_ulonglong, value));
+                const converted_val = @as(c_ulonglong, value);
+                _ = std.c.printf("%llu", converted_val);
+                c.SYS_Report("%llu", converted_val);
             }
         },
-        .float => _ = std.c.printf("%f", @as(f64, value)),
-        .bool => _ = std.c.printf("%s", if (value) "true".ptr else "false".ptr),
+        .float => {
+            const converted_val = @as(f64, value);
+            _ = std.c.printf("%f", converted_val);
+            c.SYS_Report("%f", converted_val);
+        },
+        .bool => {
+            _ = std.c.printf("%s", if (value) "true".ptr else "false".ptr);
+            c.SYS_Report("%s", if (value) "true".ptr else "false".ptr);
+        },
         .pointer => |ptr_info| {
             if (ptr_info.size == .slice and ptr_info.child == u8) {
                 _ = std.c.printf("%.*s", @as(c_int, @intCast(value.len)), value.ptr);
+                c.SYS_Report("%.*s", @as(c_int, @intCast(value.len)), value.ptr);
             } else if (ptr_info.size == .One and ptr_info.is_const and ptr_info.child == [ptr_info.sentinel orelse 0:0]u8) {
                 _ = std.c.printf("%s", value);
+                c.SYS_Report("%s", value);
             } else {
                 _ = std.c.printf("%p", value);
+                c.SYS_Report("%p", value);
             }
         },
         .error_set => {
             _ = std.c.printf("%s", @errorName(value).ptr);
+            c.SYS_Report("%s", @errorName(value).ptr);
         },
         else => @compileError("Unable to print value"),
     }
